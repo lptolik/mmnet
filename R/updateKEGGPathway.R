fd3fc3935af18304eeef34a32cab07d0e984199d
A Sorokin <lptolik@gmail.com>
2017-05-24 14:12:30 +0300

    add more pathways for consideration and preserve pathway/KO mapping

--- R/updateKEGGPathway.R
+++ R/updateKEGGPathway.R
@@ -1,12 +1,13 @@
 updateKEGGPathway <- function(path = Sys.getenv("HOME")) {
     ko.list <- keggList("pathway/ko")
     deleteGlobal.info <- regexpr("ko01|ko00", names(ko.list), perl = TRUE)
-    ko.list <- ko.list[deleteGlobal.info > 0]
+    ko.list <- ko.list[deleteGlobal.info > 0|(grepl('(B|b)acter',ko.list)&!grepl('(E|e)pithel',ko.list))]
     ko.path <- regexpr("ko\\d+", names(ko.list), perl = TRUE)
-    ko.pathway <- substring(names(ko.list), ko.path, ko.path + attr(ko.path, "match.length") - 
+    ko.pathway <- substring(names(ko.list), ko.path, ko.path + attr(ko.path, "match.length") -
         1)
-    RefDbcache <- lapply(ko.pathway, getKOPathwayInfo)
-    names(RefDbcache) <- ko.pathway
-    RefDbcache <- as.list(preprocessKOMetabolites(RefDbcache))
+    pathway.info <- lapply(ko.pathway, getKOPathwayInfo)
+    names(pathway.info) <- ko.pathway
+    RefDbcache <- as.list(preprocessKOMetabolites(pathway.info))
+    RefDbcache$pathway<-pathway.info
     saveMetabolicData(RefDbcache, path)
-} 
+}
